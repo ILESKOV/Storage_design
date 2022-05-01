88ca1b52c970cbd90c550dac26ae2c075326f2d1
ILESKOV <103837756+ILESKOV@users.noreply.github.com>
2022-05-01 23:34:04 +0200

    Update array_w_unique_ids.sol

--- array_w_unique_ids.sol
+++ array_w_unique_ids.sol
@@ -1,21 +1,34 @@
+// This is improve of simple storage patern with using mapping of address => bool
+// in order to solve problem with having duplicates data in array
+
 pragma solidity 0.8.0;
 
 contract arrayWithUniqueIds {
 
+// We have a struct that contain some data
   struct EntityStruct {
     address entityAddress;
     uint entityData;
   }
 
+// We stored this structs in an array
   EntityStruct[] public entityStructs;
+  
+// With this we can know has this address already enter the data into the mapping
   mapping(address => bool) knownEntity;
 
   function newEntity(address entityAddress, uint entityData) public returns(uint rowNumber) {
+ 
+ // and if it have once we create an entity we check this and revert if it's not the case
     if(isEntity(entityAddress)) revert();
+    
+    // Creating new entity
     EntityStruct memory newEntity;
     newEntity.entityAddress = entityAddress;
     newEntity.entityData = entityData;
     knownEntity[entityAddress] = true;
+    
+    // Pushing it to the array
     return entityStructs.push(newEntity) - 1;
   }
 
@@ -34,3 +47,15 @@ contract arrayWithUniqueIds {
     return entityStructs.length;
   }
 }
+
+/* 
+Benefits of this type of pattern :
+       - It's simple
+       - Everything that we save would be in a chronological order
+       - We can see how many things we've been saved instead of mapping, where we can't check the sum of all data
+       
+Drawbacks of this type of pattern :
+       - We could input the id and get struct data, but we can't input address and get back the struct, 
+       instead we must search throw with the loop looking for the entity that has my address. This is VERY costly
+       - There is no good easy solution to delete data from an array in this case
+*/
