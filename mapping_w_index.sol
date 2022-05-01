a9232d602e0d7af2243901787ce9793a8481541a
ILESKOV <103837756+ILESKOV@users.noreply.github.com>
2022-05-01 23:45:52 +0200

    Update mapping_w_index.sol

--- mapping_w_index.sol
+++ mapping_w_index.sol
@@ -1,13 +1,21 @@
+// - We can't get enumerates throw the KEY-VALUE to get all of entities
+
 pragma solidity 0.8.0;
 
 contract MappedStructsWithIndex {
 
-  struct EntityStruct {
+// We have a struct that contain some data
+struct EntityStruct {
     uint entityData;
+    // This bool type is for distinguish between data that we actively set to 0 or mapping that doesn't exist
     bool isEntity;
   }
 
+// We have mapping
   mapping(address => EntityStruct) public entityStructs;
+  
+// And we have array that save al of the KEYS of addresses to the mapping
+// 
   address[] public entityList;
   
   function newEntity(address entityAddress, uint entityData) public returns(uint rowNumber) {
@@ -31,3 +39,18 @@ contract MappedStructsWithIndex {
     return entityList.length;
   }
 }
+
+/* 
+Benefits of this type of pattern :
+       - We don't have dublication, now we can directly get the entity of the address. No needs of looping to get data
+       - Now we can count the number of inserted values into the mapping(number of users or entities etc.)
+       - Now we can enumarate this data(loop)
+       
+Drawbacks of this type of pattern :
+       - We don't know how many of this entities is in the mapping, because we can't loop throw them all
+       and there are no such a thins like "length of the mapping", because it's just a KEY-VALUE type storage.
+       This is a big drawback depending of what we need of course
+       - There are always data in the mapping. Regardless of the key we use if we haven't set the mapping 
+       it will be set to the initial value, which is 0 for uint, empty string for string, false for bool and so on.
+       - So we need to add extra data and it's just sort messy to work with
+*/
