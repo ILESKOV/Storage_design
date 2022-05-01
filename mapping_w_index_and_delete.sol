cd239de3e872f970366be17d1ace318773f3d64d
ILESKOV <103837756+ILESKOV@users.noreply.github.com>
2022-05-02 00:38:43 +0200

    Update mapping_w_index_and_delete.sol

--- mapping_w_index_and_delete.sol
+++ mapping_w_index_and_delete.sol
@@ -105,5 +105,6 @@ Drawbacks of this type of pattern :
        - There are always data in the mapping. Regardless of the key we use if we haven't set the mapping 
        it will be set to the initial value, which is 0 for uint, empty string for string, false for bool and so on.
        - So we need to add extra data and it's just sort messy to work with
+       - Changing bool isEntity to uint listPointer, because bool is just 1 bit and tekr less space, uint take more space 
        - More complex code
 */
